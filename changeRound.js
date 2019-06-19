255c911a16aa850c7359f3b5af8eff2b01eaf059
michael-small <small203@morris.umn.edu>
2019-06-19 15:54:40 -0500

    Refactor HTML/CSS
    Put like things like buttons in one div

--- changeRound.js
+++ changeRound.js
@@ -5,13 +5,13 @@ AFRAME.registerComponent('markerhandler', {
         const button1 = document.querySelector("#next-round-button-next");
 
 
-        button.addEventListener('click', function(ev, target){
+        button.addEventListener('click', function(){
             const src = button.getAttribute('nextRoundIMG');
             document.querySelector("#round-change-a-image").setAttribute('src', src)
         });
 
 
-        button1.addEventListener('click', function(ev, target){
+        button1.addEventListener('click', function(){
             const src = button1.getAttribute('nextRoundIMG');
             document.querySelector("#round-change-a-image").setAttribute('src', src)
         });
