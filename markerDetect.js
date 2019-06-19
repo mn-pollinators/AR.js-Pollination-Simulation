f4c91a4e752e758e0718167c7881b0da03cb0cdf
michael-small <small203@morris.umn.edu>
2019-06-19 16:57:23 -0500

    Make marker detect status change on detection change
    With just a little DOM tinkering, the detection status shows up easily
    on screen.

--- markerDetect.js
+++ markerDetect.js
@@ -2,17 +2,24 @@
 //https://github.com/jeromeetienne/AR.js/blob/6350b439077bd67b11999114fb79050e532bc969/aframe/examples/marker-events.html#L7
 
 AFRAME.registerComponent('registerevents', {
+
     init: function () {
+
+        const detectStatusDiv = document.querySelector("#detect-status");
+        detectStatusDiv.innerHTML = "Marker: Not Found";
+
         var marker = this.el;
         marker.addEventListener('markerFound', function() {
             var markerId = marker.id;
             console.log('markerFound', markerId);
-            // TODO: Add your own code here to react to the marker being found.
+
+            detectStatusDiv.innerHTML = "Marker: Found";
         });
         marker.addEventListener('markerLost', function() {
             var markerId = marker.id;
             console.log('markerLost', markerId);
-            // TODO: Add your own code here to react to the marker being lost.
+
+            detectStatusDiv.innerHTML = "Marker: Lost";
         });
     }
 });
